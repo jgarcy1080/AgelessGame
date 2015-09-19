fac8d7858dba05de7e5281a4a87a823028979490
SGTMcClain <n8mcclain@gmail.com>
2015-09-19 13:25:05 -0400

    Timer added
    Counts up not down though... :-1:

---
+++ Assets/Scripts/CountdownTimer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CountdownTimer : MonoBehaviour {
+
+	public Text countdownText;
+	public float seconds, minutes;
+	//public float totalTime = 60f;
+	// Use this for initialization
+	void Start () {
+		countdownText = GetComponent<Text>() as Text;
+		}
+	
+	// Update is called once per frame
+	void Update () {
+		minutes = (int)(Time.timeSinceLevelLoad / 60f);
+		seconds = (int)(Time.timeSinceLevelLoad % 60f);
+		countdownText.text = minutes.ToString("00") + ":" + seconds.ToString("00");
+	
+	}
+}
