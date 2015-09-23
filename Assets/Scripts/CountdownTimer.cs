b49956602a337f3b679acf287c8298b80be69b23
SGTMcClain <n8mcclain@gmail.com>
2015-09-23 19:04:28 -0400

    Timer Working in Scene

--- Assets/Scripts/CountdownTimer.cs
+++ Assets/Scripts/CountdownTimer.cs
@@ -4,21 +4,28 @@ using UnityEngine.UI;
 
 public class CountdownTimer : MonoBehaviour {
 
-	public Text countdownText;
-	public static float seconds;
-	public static float minutes;
-	//public float timeLimit = 5f;
-	// Use this for initialization
-	void Start () {
-		countdownText = GetComponent<Text>() as Text;
-		}
-	
-	// Update is called once per frame
-	void Update () {
-		minutes = (int)(Time.timeSinceLevelLoad / 60f);
-		seconds = (int)(Time.timeSinceLevelLoad % 60f);
-		countdownText.text = minutes.ToString("00") + ":" + seconds.ToString("00");
+    public Text countdownText;
+    float seconds, minutes;
+    //This variable adujst the amount of time allowed for the level.
+    public float lengthOfTime;
 
-	
-	}
+    //public float totalTime = 60f;
+    // Use this for initialization
+    void Start()
+    {
+        countdownText = GetComponent<Text>() as Text;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //To get the clock to display as a normal clock we divide timeSinceLevelLoad by 60 and the remainder by 60
+        //The negative sign in front of time is there to make sure the clock is running backwards
+        //lengthOfTime is the variable defined above that is adjustable for the level
+        //Adding 60f to secondsRemaining prevents the seconds from displaying a negative number
+        minutes = (int)((-Time.timeSinceLevelLoad / 60f) + lengthOfTime);
+        seconds = (int)((-Time.timeSinceLevelLoad % 60f) + 60f);
+        countdownText.text = minutes.ToString("00") + ":" + seconds.ToString("00");
+
+    }
 }
