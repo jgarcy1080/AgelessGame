36bb219beb0504aaecf4831930fc7b47b2b685c0
Nathan McClain <n8mcclain@gmail.com>
2015-09-26 05:22:08 -0400

    Basic Pause Implementation
    Countdown Timer now is clamped to 0 meaning that it will never show a
    negative time.
    
    Pause function created when minutes are equal to 0 and seconds are less
    than 1 second.  Currently the game has a resume function (just in case)
    though it isn't implemented yet.
    
    Next Step: Create a Restart Level UI that allows the player to reset the
    level if time runs out.  This UI may also be able to show certain stats
    at a future time.

--- Assets/Scripts/CountdownTimer.cs
+++ Assets/Scripts/CountdownTimer.cs
@@ -5,9 +5,13 @@ using UnityEngine.UI;
 public class CountdownTimer : MonoBehaviour {
 
     public Text countdownText;
-    float seconds, minutes;
+    static float seconds, minutes;
+    //
     //This variable adujst the amount of time allowed for the level.
-    public float lengthOfTime;
+    //Since seconds are added at runtime minutes will appear as one minute less than
+    //minutes inserted i.e. 5 minute timer will display 4:59 at runtime
+    public float lengthOfTimeInMinutes = 5f;
+    
 
     //public float totalTime = 60f;
     // Use this for initialization
@@ -23,9 +27,30 @@ public class CountdownTimer : MonoBehaviour {
         //The negative sign in front of time is there to make sure the clock is running backwards
         //lengthOfTime is the variable defined above that is adjustable for the level
         //Adding 60f to secondsRemaining prevents the seconds from displaying a negative number
-        minutes = (int)((-Time.timeSinceLevelLoad / 60f) + lengthOfTime);
+        minutes = (int)(Mathf.Clamp(((-Time.timeSinceLevelLoad / 60f) + lengthOfTimeInMinutes), 0f, lengthOfTimeInMinutes)); //Mathf.Clamp() keeps the clock from displaying negative numbers
         seconds = (int)((-Time.timeSinceLevelLoad % 60f) + 60f);
+
+        //make the clock stop at 00:00
+        if (minutes == 0 && seconds >= 1)
+        {
+            seconds = 0;
+            PauseGame();
+        }
+
         countdownText.text = minutes.ToString("00") + ":" + seconds.ToString("00");
 
+        
+    }
+
+    public static void PauseGame()
+    {
+
+        Time.timeScale = 0;
+        
+    }
+
+    public static void ResumeGame()
+    {
+        Time.timeScale = 1;
     }
 }
